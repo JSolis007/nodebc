5d5f27ce7e96a1ec629938fad779df7052f40c07
JSolis <jerez.solis@gmail.com>
2021-01-25 23:40:37 -0800

    adding  more js exercises

---
+++ js/index47.js
@@ -0,0 +1,4 @@
+let students = 'Homer, Marge, Bart, Lisa, Maggie';
+let result = students.split(', ');
+
+console.log(`Students names: ${result}`);
---
+++ js/index49.js
@@ -0,0 +1,6 @@
+let firstNumber = 10;
+let secondNumber = 5;
+
+if (firstNumber > secondNumber) {
+  console.log(`${firstNumber} is bigger than ${secondNumber}`);
+}
