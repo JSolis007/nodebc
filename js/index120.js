5b57dedcaf133891234cae97068b77ae0fab069b
JSolis <jerez.solis@gmail.com>
2021-01-27 12:50:07 -0800

    added more js exercises

---
+++ js/index120.js
@@ -0,0 +1,12 @@
+const students = [];
+
+students[0] = 'Homer';
+students[1] = 'Marge';
+students[2] = 'Bart';
+students[3] = 'Lisa';
+students[4] = 'Maggie';
+students[5] = 'Grandpa Simpson';
+
+for (let i = 0; i < students.length; i++) {
+  console.log(`index ${i}: ${students[i]}`);
+}
