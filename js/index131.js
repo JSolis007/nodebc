197a7da598994630702f1878ed835fb886dcc91a
JSolis <jerez.solis@gmail.com>
2021-01-27 20:24:42 -0800

    added more js exercises

--- js/index131.js
+++ js/index131.js
@@ -2,5 +2,25 @@ const femaleStudents = ['Marge', 'Lisa', 'Maggie', 'Aunt Selma', 'Aunt Pattie'];
 const maleStudents = ['Homer', 'Bart', 'Grandpa Simpson', 'Mr. Burns', 'Ned Flanders'];
 
 const students = femaleStudents.concat(maleStudents);
+students.sort();
 
-console.log(students);
+console.log(femaleStudents[0]);
+console.log(femaleStudents[1]);
+console.log(femaleStudents[2]);
+console.log(femaleStudents[3]);
+console.log(femaleStudents[4]);
+console.log('\n');
+
+let i = 0;
+while (i < maleStudents.length) {
+  console.log(maleStudents[i]);
+  i++
+}
+
+console.log('\n');
+
+
+for (let i = 0; i < students.length; i++) {
+  console.log(students[i]);
+}
+
---
+++ js/index133.js
@@ -0,0 +1,26 @@
+let mutants = [
+  'Professor X',
+  'Cyclops',
+  'Iceman',
+  'Angel',
+  'Magneto',
+  'Beast',
+  'Phoenix',
+  'Logan',
+  'Gambit'
+];
+
+for (let i = 0; i < mutants.length; i++) {
+  if (mutants[i] === 'Magneto') {
+    break;
+  }
+  console.log(mutants[i]);
+}
+
+console.log('\n');
+
+mutants = mutants.filter(mutant => mutant !== 'Magneto');
+
+for (let i = 0; i < mutants.length; i++) {
+  console.log(`${i}. ${mutants[i]}`);
+}
