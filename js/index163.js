a1c957f75627aaa9424d20ceab9c9dabc16def04
JSolis <jerez.solis@gmail.com>
2021-01-28 15:07:38 -0800

    added more js exercises

---
+++ js/index163.js
@@ -0,0 +1,20 @@
+function add() {
+  let nums = arguments;
+  let result = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    result += nums[i];
+  }
+  if (nums.length >= 1) {
+    console.log(result);
+
+  } else {
+    console.log('No numbers to add');
+  }
+}
+
+add(2, 5);
+add(2, 5, 10);
+add(2);
+add(2, 5, 10, 27, 1, 3, 1, 34, 12, 9);
+add();
