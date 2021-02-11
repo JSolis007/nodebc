b15adbacbea1622a5662d75e8daa0e30949ef532
mochimoko <jerez.solis@gmail.com>
2021-02-10 23:25:55 -0800

    added more nodejs exercises

---
+++ nodejs/ex_12/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/api/products', (req, res) => {
+  res.json({
+    description: "Products",
+    items: [
+      { name: "Star Wars jacket", price: 100 },
+      { name: "FIFA 22 PS4", price: 79 },
+      { name: "Superheore t-shirt", price: 10 },
+      { name: "Jets game shirt", price: 200 },
+      { name: "Large Meat lovers pizza", price: 12 },
+      { name: "Canada Dry  bottle", price: 2 },
+    ],
+  });
+});
+
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
